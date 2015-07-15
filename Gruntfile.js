f208f74ad0c8475f00c4a7a46e76ebe8ea3d6c4b
Daniele Stroppa <daniele.stroppa@gmail.com>
2015-07-15 13:54:43 +0100

    updated grunt-aws-plugin to version 0.8.0

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,18 +2,23 @@ var grunt = require('grunt');
 grunt.loadNpmTasks('grunt-aws-lambda');
 
 grunt.initConfig({
-    lambda_invoke: {
-        default: {
-            options: {
-                file_name: 'CreateThumbnail.js'
-            }
-        }
+	lambda_invoke: {
+    	default: {
+        	options: {
+            	file_name: 'CreateThumbnail.js'
+        	}
+    	}
+	},
+	lambda_deploy: {
+    	default: {
+    		arn: 'arn:aws:lambda:us-east-1:1234567890123:function:CreateThumbnail'
+    	}
     },
-    lambda_package: {
-        default: {
-            options: {
-                include_time: false
-            }
-        }
-    }
+	lambda_package: {
+    	default: {
+    		include_time: false
+    	}
+	}
 });
+
+grunt.registerTask('deploy', ['lambda_package', 'lambda_deploy'])
